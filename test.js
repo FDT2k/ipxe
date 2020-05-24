ad7b802c971a413cbb70ab47676a6f4a40fcaa89
Fabien Di Tore <fabien@ditore.ch>
2020-05-24 21:09:44 +0200

    Update .gitignore, general.h, and 2 more files...

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ import { serve } from "https://deno.land/std/http/server.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 import {compose,curry,map} from '../gka-composite/src/core.js'
-import {replace} from '../gka-composite/src/string.js'
-import {reduce} from '../gka-composite/src/array.js'
+import {replace,concat} from '../gka-composite/src/string.js'
+import {reduce,joinList} from '../gka-composite/src/array.js'
+import {trace} from '../gka-composite/src/debug.js'
 import {assign2} from '../gka-composite/src/object.js'
 //await init();
 
@@ -26,13 +27,44 @@ const toObject =  curry((item)=>({[item[0]]:decodeValue(item[1])}) )
 const getQS = compose(toObject,value)
 
 const querystring = url => {
+
   const [baseUrl,querystring] =urlParts(url)
-  const parts = split('&',querystring)
+  const parts = split('&',querystring || '')
+  if( parts.length ===1 && parts[0] ===""){
+    return {}
+  }
   return mergeAll(parts.map(getQS))
 }
 
+const ipxe_header = _=>`#!ipxe\n`
+
+const ipxe_menu = name => `menu ${name}`;
+const ipxe_menuitem = key=> label => `item ${key} ${label}`;
+
+
+const ipxe_choose =  str=> `choose ${str}`;
+const ipxe_default_choose = key => ` --default ${key} `;
+const ipxe_choose_target = varname => `${varname} && goto ${varname}`;
+
+const ipxe_menu_default_choose = default_key =>   compose(ipxe_choose,concat(ipxe_default_choose(default_key)),ipxe_choose_target)
+
+const ipxe_section = section => str => `${section}:\n${str}`
+
+const menu = _=> joinList('\n',[
+  ipxe_header(),
+  ipxe_menu('Test'),
+  ipxe_menuitem('key')('Label'),
+  ipxe_menuitem('test')('Test'),
+  ipxe_menu_default_choose('test')('target')
+])
+
 for await (const req of s) {
+  console.log(req.url)
   const params = querystring(req.url)
+  console.log(req.body)
   console.log(params)
-  req.respond({ body: "#!ipxe\nmenu Test\nitem test Test\n choose --default test target && goto target" });
+  console.log(menu())
+  req.respond({
+    body: menu()
+  })
 }
